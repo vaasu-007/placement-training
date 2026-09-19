a85cd9900f288acf323e5775f377905677db2a01
vaasu-007 <vasanth.k_25am@kgkite.ac.in>
2026-09-19 23:56:37 +0530

    Perfect number code has been added

---
+++ Task_01/PerfectNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == n)
+            System.out.println(n + " is a Perfect Number.");
+        else
+            System.out.println(n + " is Not a Perfect Number.");
+    }
+}
