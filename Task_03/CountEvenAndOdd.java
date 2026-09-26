c960b6fe22eeb117ceee12d35ce1e0403f9bba9c
vaasu-007 <vasanth2102007@gmail.com>
2026-09-26 10:20:09 +0530

    counting even and odd ccode is added

---
+++ Task_03/CountEvenAndOdd.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CountEvenAndOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        int evenCount = 0;
+        int oddCount = 0;
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+                if (matrix[i][j] % 2 == 0) {
+                    evenCount++;
+                } else {
+                    oddCount++;
+                }
+            }
+        }
+
+        System.out.println("Even elements count: " + evenCount);
+        System.out.println("Odd elements count: " + oddCount);
+    }
+}
